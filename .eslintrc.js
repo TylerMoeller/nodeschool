ff80c9d96e3127d9458ca6ef95331f3b79dca9e2
Tyler Moeller <tylermoeller1@gmail.com>
2018-06-12 17:11:49 -0700

    Update Line Endings

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": false,
+        "commonjs": true,
+        "es6": true,
+        "jquery": false
+      },
+      "rules": {
+          "no-console": 0,
+          "consistent-return": 0,
+          "no-plusplus": 0,
+          "import/no-unresolved": 0,
+          "no-param-reassign": 0
+      }      
+}
